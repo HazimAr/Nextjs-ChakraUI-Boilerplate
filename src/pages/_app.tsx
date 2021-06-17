6c75eb082ff89a613a34895bb7897e95b6c8c662
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-17 08:58:29 -0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@styles/global.css";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 import Footer from "@components/footer";
 import Header from "@components/header";
 import { pageview } from "@lib/gtag";
