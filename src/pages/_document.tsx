187e4f2c6c884732f71f45b53b5b999d25e6ff4b
Hazim Arafa <hazimarafa80@gmail.com>
2021-08-31 12:46:25 -0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,12 @@ export default class MyDocument extends Document {
 			<Html lang="en-us">
 				<Head>
 					<meta name="description" content={META.description} />
+					<meta name="author" content="https://webdefy.tech" />
+					<meta name="keywords" content={META.tags.join(",")} />
 
 					<meta itemProp="name" content={META.title} />
 					<meta itemProp="description" content={META.description} />
 					<meta itemProp="image" content={META.image} />
-					<meta name="author" content="https://webdefy.tech"/>
-					<meta name="keywords" content={META.tags.join(",")} />
 
 					<meta property="og:url" content={META.url} />
 					<meta property="og:type" content="website" />
