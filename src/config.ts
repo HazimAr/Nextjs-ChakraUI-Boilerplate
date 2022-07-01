fc7cc1fd8e8de101d63368e2d03aa779294315d1
Hazim Arafa <hazimarafa80@gmail.com>
2022-07-01 03:36:03 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,7 @@ const META = {
 	description:
 		"Boilerplate built to scale containing Typescript + NextJS + ChakraUI + Google Analytics + ESLint + Jest + Styled Components + Icons",
 	url: "https://boilerplate.hazim.tech",
-	image: "/logo.png",
-	tags: ["nextjs", "typescript", "chakraui", "google analytics", "eslint", "jest", "styled components", "icons"]
+	image: "/logo.png"
 };
 
 export { GA_TRACKING_ID, META };
